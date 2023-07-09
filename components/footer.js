5f79335bc07d24fa3cbf41695b8eb3cda1478441
Kenny Ng <ngkailun@NGs-MacBook-Pro.local>
2023-07-09 23:51:28 +0100

    Completed <header>

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </>
+  );
+}
